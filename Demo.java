a506f9eb3f83a0cb028f21309633b8898176e983
shivam <shivampoddartech@gmail.com>
2025-03-01 22:51:10 +0530

    adding windin in type casting

---
+++ Demo.java
@@ -0,0 +1,26 @@
+package typeCasting;
+
+public class Demo {
+	public static void main(String[] args) {
+
+		int a = 10;
+		double b = a;
+		System.out.println(a + " " + b);
+
+		System.out.println("----------");
+
+		int c = 3600;
+		float d = c;
+		System.out.println(c + " " + d);
+
+	}
+}
+/*
+what is type casting -  A Process of converting one Data Type To Another Data Type  Is Called Type casting 
+There Are Two Type 1 is permeative And 2 Is nonPrematve 
+1 Permeative --- Permeative have two Type Narrowing And Second is Winding
+Winding  is a process to convert small type to big type like byte short int long float double
+Narrowing  is a process to convert big type to small type is called Narrowing example is
+double float long int short byte
+
+*/
